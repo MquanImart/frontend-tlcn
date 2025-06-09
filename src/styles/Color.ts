04571583283bd6782127149815d2909f92f1a2e1
Bảo Quốc <21110741@student.hcmute.edu.vn>
2025-06-09 23:55:50 +0700

    commit

--- src/styles/Color.ts
+++ src/styles/Color.ts
@@ -1,61 +1,82 @@
 // src/styles/Color.ts
-
 export interface Color {
-  white_homologous: string;
-  white_contrast: string;
-  mainColor1: string;
-  mainColor2: string;
-  backGround: string;
-  backGround1: string;
-  backGround2: string;
-  backGround3: string;
-  backGround4: string;
-  textColor1: string;
-  textColor2: string;
-  textColor3: string;
-  textColor4: string;
-  textColor5: string;
-  borderColor1: string;
-  borderColorwb: string;
-  inputBackGround: string;
+    mainColor1: string;
+    mainColor2: string;
+    white: string;
+    black: string;
+    gray1: string;
+    gray2: string;
+    gray3: string;
+    gray4: string;
+    red_red: string;
+
+
+    white_homologous: string;
+    white_contrast: string;
+    backGround: string;
+    backGround1: string;
+    backGround2: string;
+    backGround3: string;
+    textColor1: string;
+    textColor2: string;
+    textColor3: string;
+    textColor4: string;
+    borderColor1: string;
+    borderColorwb: string;
+    inputBackGround: string;
+
 }
 
 export const lightColor: Color = {
-  white_homologous: "#fff",
-  white_contrast: "#000",
-  mainColor1: "#4B164C",
-  mainColor2: "#DD88CF",
-  backGround: "#fff",
-  backGround1: "#e9e9e9",
-  backGround2: "#f0f0f0",
-  backGround3: "#ffffff30",
-  backGround4: "#50A7E7",
-  textColor1: "#000",
-  textColor2: "#fff",
-  textColor3: "#A5ACB8",
-  textColor4: "#3d3d3d",
-  textColor5: "#FCF3FA",
-  borderColor1: "#A5ACB8",
-  borderColorwb: "#000",
-  inputBackGround: "#F0F0F0",
+    mainColor1: "#4B164C",
+    mainColor2: "#DD88CF",
+    white: "#fff",
+    black: "#000",
+    gray1: "#f0f0f0",
+    gray2: "#e9e9e9",
+    gray3: "#3d3d3d",
+    gray4: "#9E9E9E",
+    red_red: "#ff0000",
+
+
+    white_homologous: "#fff",
+    white_contrast: "#000",
+    backGround: "#fff",
+    backGround1: "#e9e9e9",
+    backGround2: "#f0f0f0",
+    backGround3: "#50A7E7",
+    textColor1: "#000",
+    textColor2: "#fff",
+    textColor3: "#A5ACB8",
+    textColor4: "#3d3d3d",
+    borderColor1: "#A5ACB8",
+    borderColorwb: "#000",
+    inputBackGround: "#F0F0F0",
 };
 
 export const darkColor: Color = {
-  white_homologous: "#000",
-  white_contrast: "#fff",
-  mainColor1: "#DD88CF",
-  mainColor2: "#4B164C",
-  backGround: "#000",
-  backGround1: "#161616",
-  backGround2: "#0a0a0a",
-  backGround3: "#ffffff30",
-  backGround4: "#50A7E7",
-  textColor1: "#fff",
-  textColor2: "#000",
-  textColor3: "#fff",
-  textColor4: "#3d3d3d",
-  textColor5: "#FCF3FA",
-  borderColor1: "#A5ACB8",
-  borderColorwb: "#fff",
-  inputBackGround: "#F0F0F0",
+    mainColor1: "#DD88CF",
+    mainColor2: "#4B164C",
+    white: "#000",
+    black: "#fff",
+    gray1: "#9E9E9E",
+    gray2: "#BDBDBD",
+    gray3: "#E0E0E0",
+    gray4: "#F0F0F0",
+    red_red: "#ff0000",
+
+
+    white_homologous: "#000",
+    white_contrast: "#fff",
+    backGround: "#000",
+    backGround1: "#161616",
+    backGround2: "#0a0a0a",
+    backGround3: "#50A7E7",
+    textColor1: "#fff",
+    textColor2: "#000",
+    textColor3: "#fff",
+    textColor4: "#3d3d3d",
+    borderColor1: "#A5ACB8",
+    borderColorwb: "#fff",
+    inputBackGround: "#F0F0F0",
 };
